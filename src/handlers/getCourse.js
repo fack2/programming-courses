b217ec672b50fbdf287e7908be814ef912367a89
Rand Inaim <rand.inaim95@gmail.com>
2019-08-08 11:07:55 +0300

    redirect when id not exists

--- src/handlers/getCourse.js
+++ src/handlers/getCourse.js
@@ -3,9 +3,13 @@ const { getCoursesById } = require('../database/queries/getDataId')
 exports.getCourseById = (req, res, next) => {
 	getCoursesById(req.params.id)
 		.then((course) => {
-			res.render('course', {
-				courses: course
-			})
+			if (course.length > 0) {
+				res.render('course', {
+					courses: course
+				})
+			} else {
+				res.redirect('/')
+			}
 		})
 		.catch((err) => {
 			next(err)
